1c512bc2af6ab95bab29f246e4e1f1a2c263daaf
IvanHolovakha <meisuiwanreiser@gmail.com>
2022-12-04 19:26:56 +0200

    changes in 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,8 +6,6 @@ const galleryRef = document.querySelector(".gallery");
 
 createGaleryMarkup(galleryItems);
 
-galleryRef.addEventListener("click", onGalleryItemClick);
-
 function createGaleryMarkup (galleryItems) {
     const markup = galleryItems.map(({preview, original, description}) => {
         return `
@@ -21,16 +19,9 @@ function createGaleryMarkup (galleryItems) {
         `;
     }
 ).join("");
-    galleryRef.insertAdjacentHTML("beforeend", markup);
-};
-function onGalleryItemClick (event) {
-    event.preventDefault();
-    if(event.target.nodeName !== "IMG") {
-        return;
-    }
-    const lightbox = new SimpleLightbox('.gallery a', {
+
+const lightbox = new SimpleLightbox('.gallery a', {
         captionsData: "alt",
         captionPosition: "bottom",
         captionDelay: 250,
     });
-}
